cfc41239ca6a1a8f998957dd2d1fd8bb685093db
Theam1127 <yeaptheam97@gmail.com>
2018-04-28 23:12:14 +0800

    New Update

--- app/src/main/java/my/edu/tarc/sciencepark/MainMenu.java
+++ app/src/main/java/my/edu/tarc/sciencepark/MainMenu.java
@@ -1,9 +1,12 @@
 package my.edu.tarc.sciencepark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -11,12 +14,23 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Button buttonOrderFood = (Button)findViewById(R.id.btnOrderFood);
+        Button buttonOrderStatus = (Button)findViewById(R.id.btnOrderStatus);
+        buttonOrderFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this,RestaurantList.class);
+                startActivity(intent);
+            }
+        });
+        buttonOrderStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, OrderStatus.class);
+                startActivity(intent);
+            }
+        });
     }
-
-     /* public boolean onOptionItemSelected(MenuItem item){
-        Intent intent1 = new Intent(this, this);
-
-    } */
 
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
