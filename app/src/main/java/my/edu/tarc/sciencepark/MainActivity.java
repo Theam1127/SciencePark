73f327be77242dda18ad6c496adc364ff276a83e
yongj <yongjin97@hotmail.com>
2018-04-28 20:37:26 +0800

    UI

--- app/src/main/java/my/edu/tarc/sciencepark/MainActivity.java
+++ app/src/main/java/my/edu/tarc/sciencepark/MainActivity.java
@@ -1,7 +1,11 @@
 package my.edu.tarc.sciencepark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
